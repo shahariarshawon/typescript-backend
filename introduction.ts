eeb86ecd72fd20ffaece1fab94f7aa91087d74c9
shahariarshawon <shahariararafat7739@gmail.com>
2026-05-07 16:16:10 +0600

    Introduction of typescript

---
+++ introduction.ts
@@ -0,0 +1,9 @@
+/*Typescript is a superset of javascript.
+1. Why we use typescript?
+-> It gives us type safety
+-> It helps us to identify bug in the runtime.
+-> Large scale projects are built using TypeScript cause it is very helpful to read and identify bugs.
+-> It helps to improve team collaboration
+-> Any javascript project can be upgraded typescript if you have strong knowldege about javascript
+
+*/
