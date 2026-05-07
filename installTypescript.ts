d92637d33469257973855865fd9a73879912473f
shahariarshawon <shahariararafat7739@gmail.com>
2026-05-07 16:53:22 +0600

    Installation setup documentation

---
+++ installTypescript.ts
@@ -0,0 +1,10 @@
+/*
+1. First install nodejs using nvm package
+2. If computer had nodejs installed already (without using NVM package) then uninstall it must.
+3. If you don't have nvm package installed in computer then install it.
+4. After installing nvm in command run "NVM install [nodejs version]". You can install more than one nodejs version
+5. you can see all the list of the nodejs by using "nvm list" command
+6. You can check either node js installed or not by using "node --version"
+7. Now install typeScript from its official documentation.
+8. TypeScript nowadays comes with default tsc features by using tsc it doesnt need to convert the ts to js file and then run , it can run the ts file directly.
+*/ 
